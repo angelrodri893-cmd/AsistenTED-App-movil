c2006aca9519ec81f1c5349ee92b8fba36119ced
alexis <angelrodri893@gmail.com>
2026-06-28 20:26:12 -0500

    feat: rename de variables y archivos

--- app/src/main/java/com/asistented/app/MainActivity.kt
+++ app/src/main/java/com/asistented/app/MainActivity.kt
@@ -1,23 +1,25 @@
-package com.asistented.app
+﻿package com.asistented.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.remember
-import com.asistented.app.ui.AsistenTedApp
-import com.asistented.app.ui.AsistenTedController
-import com.asistented.app.ui.theme.AsistenTEDv1Theme
+import com.asistented.app.interfaz.AplicacionAsistenTed
+import com.asistented.app.interfaz.ControladorAsistenTed
+import com.asistented.app.interfaz.tema.TemaAsistenTED
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val controller = remember { AsistenTedController(applicationContext) }
-            AsistenTEDv1Theme(accessibilitySettings = controller.accessibilitySettings) {
-                AsistenTedApp(controller)
+            val controlador = remember { ControladorAsistenTed(applicationContext) }
+            TemaAsistenTED(configuracionAccesibilidad = controlador.configuracionAccesibilidad) {
+                AplicacionAsistenTed(controlador)
             }
         }
     }
 }
+
+
